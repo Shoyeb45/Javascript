62bd93039241054641cde20bea9096b43d48dfcf
Shoyeb45 <shoyebff45@gmail.com>
2023-12-08 20:18:14 +0530

    new

---
+++ Notes/variable.js
@@ -0,0 +1,8 @@
+fullName = "Shoyeb Ansari"; 
+age = 19;
+height = 154.4;
+weight = 65;
+console.log("Name:",fullName);
+console.log("Age:",age);
+console.log("Height:",height);
+console.log("Weight:",weight);
