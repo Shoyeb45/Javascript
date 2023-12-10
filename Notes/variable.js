89e9541c65faf40a7cd8a5542e2373dc603b0e9c
Shoyeb45 <shoyebff45@gmail.com>
2023-12-10 20:28:49 +0530

    new

--- Notes/variable.js
+++ Notes/variable.js
@@ -1,8 +1,18 @@
-fullName = "Shoyeb Ansari"; 
-age = 19;
-height = 154.4;
-weight = 65;
+fullName = "Shoyeb Ansari"; //String value 
+age = 19;  // Integer data
+height = 154.4; //float
+weight = 65,87; //double
 console.log("Name:",fullName);
 console.log("Age:",age);
 console.log("Height:",height);
 console.log("Weight:",weight);
+
+//null and undefined variables
+
+x = null;  //No values
+y = undefined;  //don't know which value
+
+bool = true; //boolean data type
+console.log(bool);
+console.log(x);
+console.log(y);
