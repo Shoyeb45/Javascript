20c0e66d9a027942a34671d291624da7f1649d46
Shoyeb45 <shoyebff45@gmail.com>
2023-12-28 19:00:04 +0530

    Edit and new file

---
+++ Operators and conditional/Unary.js
@@ -0,0 +1,8 @@
+let a = 7, b = 9;
+
+console.log("Unary operators.");
+
+
+console.log("++a=",++a,"a++=",a++); //First ++a(++7=8) will happen and then a++(8++=8)
+console.log(a);
+//Same with unary decrement operator
---
+++ Operators and conditional/arithmetic.js
@@ -0,0 +1,12 @@
+//Simple use of arithmetic operator
+
+let a=8;
+let b=5;
+//Same as mathematics
+console.log("Addition :-> a + b = ",a+b);
+console.log("Subtraction :-> a - b = ",a-b);
+console.log("Multiplication :-> a * b = ",a*b);
+console.log("Division :-> a / b = ",a/b);
+console.log("Modulus :-> a % b = ", a%b);
+console.log("Exponentiation :-> a ** b = ",a**b); //a^b
+
