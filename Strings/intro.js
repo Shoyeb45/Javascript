0e8844e9909e479ab822d52a7c28796dd86db246
Shoyeb45 <shoyebff45@gmail.com>
2023-12-29 19:32:28 +0530

    strings

---
+++ Strings/intro.js
@@ -0,0 +1,13 @@
+let name = "Shoyeb Ansari";
+let Name = 'Mohammad Shoyeb Ansari';
+console.log("Your name is:",name);
+console.log("Your Name is:",Name);
+
+//String length
+console.log("Number of character in Your Name is:",Name.length);
+console.log("Number of character in Your name is:",name.length);
+
+//String index
+console.log("Fourth Letter of name:",name[3]);
+console.log("First letter of Name:",Name[0]);
+
---
+++ Strings/templateLiteral.js
@@ -0,0 +1,9 @@
+const product = {
+    name : "Pen",
+    price : 10,
+    ratingStar : 4
+};
+
+let description = `The name of product is ${product.name}, price is INR ${product.price} and rating is ${product.ratingStar} star.`;
+
+console.log(description);
