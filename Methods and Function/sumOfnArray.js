bc4cc0970564ac733a43066c41e31e454c96ff99
Shoyeb45 <shoyebff45@gmail.com>
2024-01-20 18:45:37 +0530

    examples

--- Methods and Function/sumOfnArray.js
+++ Methods and Function/sumOfnArray.js
@@ -1,2 +1,23 @@
+console.log("\t\t***Sum of number from 1 to n in array using reduce***")
+
 let n = prompt("Enter number")
+let number = [];
+
+for(let i = 1; i<=n ; i++){
+    number[i] = i;
+}
+
+let i = 1;
+const sum = number.reduce((prev, curr) => {
+    return (prev+curr) ; 
+});
+
+//same way we can multiply all the elements
+
+const mult = number.reduce((prev, curr) => {
+    return prev*curr; 
+})
+
+console.log("Addition",sum)
+console.log("Multiplication:",mult)
 
