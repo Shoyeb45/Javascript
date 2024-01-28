bb3b67c58b0f0b758813e42db127ad548d331f55
Shoyeb45 <shoyebff45@gmail.com>
2024-01-28 16:45:40 +0530

    property of element

---
+++ DOM/property.js
@@ -0,0 +1,9 @@
+let Div = document.querySelector("div")
+console.dir(Div)
+
+let list = document.querySelector("ul")
+console.dir(list)
+
+let heading2 = document.querySelector("h2")
+
+let hiddenElement = document.querySelector("h3")
