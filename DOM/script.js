18633b83b2594257658a7708af0f440bc9ea74e3
Shoyeb45 <shoyebff45@gmail.com>
2024-01-28 16:02:40 +0530

    DOM1

--- DOM/script.js
+++ DOM/script.js
@@ -1,14 +1,35 @@
-// document.body.childNodes[1].innerText = "Hii";
-// 1.
-let x = document.getElementById("H2")
-console.dir(x)
+// // document.body.childNodes[1].innerText = "Hii";
+// // 1.
+// let x = document.getElementById("H2")
+// console.dir(x)
 
-// 2.
-let lists = document.getElementsByClassName("list")
-console.dir(lists)
-console.log(lists)
+// // 2.
+// let lists = document.getElementsByClassName("list")
+// console.dir(lists)
+// console.log(lists)
 
-// 3.
-let p = document.getElementsByTagName("p")
-console.log(p)
-console.dir(p)
+// // 3.
+// let p = document.getElementsByTagName("p")
+// console.log(p)
+// console.dir(p)
+
+// query selector
+
+// by tagname
+let Tag = document.querySelector("p")
+let Tag2 = document.querySelectorAll("p")
+console.dir(Tag)
+console.dir(Tag2)
+
+// by id
+
+let  ID = document.querySelector("#H2")
+console.dir(H2)
+
+// by className
+let CLASS = document.querySelector(".list")
+let CLASS2 = document.querySelectorAll(".list")
+console.dir(CLASS)
+console.dir(CLASS2)
+
+let bt = document.querySelector("#myButton")
