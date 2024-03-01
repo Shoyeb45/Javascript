b8639c67dc4a758fdee001bb87dbd577f3cd38ab
Shoyeb45 <shoyebff45@gmail.com>
2024-03-01 13:28:15 +0530

    some more string method

---
+++ Strings/methodStrin2.js
@@ -0,0 +1,29 @@
+let a = "My name is Shoyeb Ansari";
+
+// 1. substr
+console.log(a.substr(0, 4));
+
+// 2. substring
+console.log(a.substring(1, 4));
+
+// 3. includes()
+console.log(a.includes("Shoyeb"));
+
+// 4. replace()
+console.log(a.replace("Shoyeb Ansari", "Prakhar Sahu"));
+
+// 5. lastindexOf()
+console.log(a.lastIndexOf("Sho"));
+
+// 6. startsWith()
+console.log(a.startsWith("My"));
+
+// 6. endsWith()
+console.log(a.endsWith("ar"));
+
+// 7. match()
+console.log(a.match("Shoyeb"));
+
+// 8. repeat()
+console.log("Hii ".repeat(8));
+
