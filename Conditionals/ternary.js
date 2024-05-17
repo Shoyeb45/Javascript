b5b38e592326af35bae0f8db472ef53aaa078cf6
Shoyeb45 <shoyebff45@gmail.com>
2024-05-17 10:10:21 +0530

    Ternary operator

---
+++ Conditionals/ternary.js
@@ -0,0 +1,15 @@
+function max(a, b) {
+    return a >= b ? a: b;
+}
+
+function min(a, b) {
+    return a <= b? a: b;
+}
+
+function absolute(a) {
+    return a >= 0? a:-a;
+}
+
+console.log(max(12 ,41));
+console.log(min(12 ,41));
+console.log(absolute(-1232));
