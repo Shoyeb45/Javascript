11f139e973f740d8e538a1986689b9294b731206
Shoyeb45 <shoyebff45@gmail.com>
2024-05-17 10:10:06 +0530

    switch

---
+++ Conditionals/switch.js
@@ -0,0 +1,18 @@
+a = 4;
+switch(a) {
+    case 1: 
+        console.log("One");
+        break;
+    case 2: 
+        console.log("Two");
+        break;
+    case 3: 
+        console.log("Three");
+        break;
+    case 4: 
+        console.log("Four");
+        break;
+    default:
+        console.log("Random")
+}
+console.log("Out of switch");
