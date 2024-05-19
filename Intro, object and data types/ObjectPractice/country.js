608fdd20dd7fdf12b8f56c1a3113df9bdcbcc69c
Shoyeb45 <shoyebff45@gmail.com>
2024-05-19 23:03:55 +0530

    Contry object

---
+++ Intro, object and data types/ObjectPractice/country.js
@@ -0,0 +1,8 @@
+const country = {
+    name: "India",
+    capital: "New Delhi",
+    population: "1.44 billions",
+    langauges: ["Assamese", "Bangla", "Bodo", "Dogri", "Gujarati", "Hindi", "Kashmiri", "Kannada", "Konkani", "Maithili", "Malayalam",     "Manipuri", "Marathi", "Nepali", "Oriya", "Punjabi", "Tamil", "Telugu", "Santali", "Sindhi", "Urdu", "English"]
+}
+
+console.log(country);
