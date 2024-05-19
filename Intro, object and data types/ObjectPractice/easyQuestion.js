d329c364f08f51e3115fa90b4484bd9c4f78315c
Shoyeb45 <shoyebff45@gmail.com>
2024-05-19 22:23:05 +0530

    Object easy - one

---
+++ Intro, object and data types/ObjectPractice/easyQuestion.js
@@ -0,0 +1,12 @@
+dog = {};
+console.log(dog);
+
+dog["name"] = "Sadaharu";
+dog["leg"] = 4;
+dog["color"] = "white";
+
+dog.bark = () => {
+    return "Woof Woof";
+}
+
+console.log(`Name : ${dog.name}\nLeg : ${dog.leg}\nColor : ${dog.color}\n${dog.name} is hungry that's why he is doing ${dog.bark()}`);
