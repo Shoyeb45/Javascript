d4f96a4a4e7b23e484cdeae0a62ff102a402fb20
Shoyeb45 <shoyebff45@gmail.com>
2025-01-02 10:42:15 +0530

    arrow function

--- Methods and Function/arrowFun.js
+++ Methods and Function/arrowFun.js
@@ -18,4 +18,36 @@ const sum = (a, b) => {
 }
 
 
+const user = {
+    name: "Shoyeb",
+    email: "shoyeb@gmail.com",
+    welcomeMsg: function() {
+        console.log(`${this.name}, welcome to website`);
+        console.log(this);
+        
+    }
+};
 
+user.welcomeMsg();
+user.name = "Durgesh";
+user.welcomeMsg();
+
+console.log(this);   // Node environment: current context is empty in node environment. But in window the current context is the object with browser functionality
+
+function someFun() {
+    let username = "shoyeb45";
+    console.log(this);
+    console.log(this.username); // No variable named `username` in the context
+}
+
+// Execute function
+someFun();
+
+
+// Arrow function:
+const f = () => {
+    let name = "navneet";
+    console.log(this);  // nothing
+};
+
+f();
