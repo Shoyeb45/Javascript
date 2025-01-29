c7cad0e7cb2a93495f4681c437be6c4ec6b1295d
Shoyeb45 <shoyebff45@gmail.com>
2025-01-29 14:44:26 +0530

    Divide by zero error

---
+++ TryCatch/divideWithZero.js
@@ -0,0 +1,13 @@
+function divide(a, b) {
+    if (b == 0) {
+        throw new Error("Can't divide a number by zero");
+    }
+    return a / b;
+}
+
+try {
+    const res = divide(12, 12341412);
+    console.log(`12 / 12341412 = ${res}`);
+} catch (error) {
+    console.log(error.message);
+}
